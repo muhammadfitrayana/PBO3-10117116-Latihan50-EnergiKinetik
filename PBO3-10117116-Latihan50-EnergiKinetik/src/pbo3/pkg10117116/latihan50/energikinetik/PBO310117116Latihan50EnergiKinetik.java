39be2ea4009d770cbab814e568c3e2226f548660
muhammadfitrayana <38598539+muhammadfitrayana@users.noreply.github.com>
2018-10-20 20:57:25 +0700

    Latihan50

---
+++ PBO3-10117116-Latihan50-EnergiKinetik/src/pbo3/pkg10117116/latihan50/energikinetik/PBO310117116Latihan50EnergiKinetik.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117116.latihan50.energikinetik;
+
+import java.text.DecimalFormat;
+
+/**
+ *
+ * @author 
+ * NAMA  : Muhammad Fitrayana
+ * Kelas : PBO3
+ * NIM   : 10117116
+ * Deskripsi Tugas : Membuat Program Menghitung Energi Kinetik dan Usaha
+ *  
+ */
+public class PBO310117116Latihan50EnergiKinetik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        EnergiKinetik kinetik = new EnergiKinetik();
+        kinetik.setMassa(0.145);
+        kinetik.setKecepatan(25);
+        kinetik.setKecepatanAwal(0);
+        kinetik.setKecepatanAkhir(25);
+        
+        DecimalFormat df = new DecimalFormat("#.#");
+        
+        System.out.println("=====PROGRAM MENCARI ENERGI KINETIK=====");
+        System.out.println("Energi Kinetik : " + df.format(
+                           kinetik.hitungEnergiKinetik
+                          (kinetik.getMassa(), kinetik.getKecepatan())));
+        System.out.println("Usaha : " + df.format (kinetik.hitungUsaha
+                          (kinetik.getKecepatanAwal(), 
+                           kinetik.getKecepatanAkhir())));
+    }
+    
+}
